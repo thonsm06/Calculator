c8cff4acbe960de3a53501606a725345f19610d2
P51 <116685524+thonsm06@users.noreply.github.com>
2022-11-03 22:19:54 -0500

    Completed basic keyboard input

--- js/javascript.js
+++ js/javascript.js
@@ -19,15 +19,211 @@ buttonColor.forEach(button => button.addEventListener('mouseleave', () => {
     button.style.boxShadow = "none";
 }))
 
+window.addEventListener('keydown', function (e) {
+    if (e.key === ' ' || e.key === "Backspace") //spacebar
+    {
+        backspace(e.key);
+    }
+    else if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+    {
+        operation(e.key);
+    }
+    else if (e.key === '=' || e.key === 'Enter')
+    {
+        equal(e.key);
+    }
+    else if (e.key >= 0 && e.key <= 9)
+    {
+        numerals(e.key);
+    }
+})
+function backspace (key) {
+    //when pressed backspace, delete last digit of currentInput
+    if (currentInput !== '')
+    {
+        currentInput = currentInput.slice(0, -1);
+        if (currentInput === '')
+        {
+            displayValue.textContent = '0';
+        }
+        else
+        {
+            displayValue.textContent = currentInput;
+        }
+        
+    }
+    log(currentInput, operator, secondValue, displayValue.textContent, displayEquation.textContent, previousButton);
+}
+function numerals(key) {
+    if (previousButton === '=')
+    {
+        if (secondValue === '0')
+        {
+            currentInput += key;
+            displayValue.textContent = currentInput;
+        }
+        else
+        {
+            clear();
+            currentInput += key;
+            displayValue.textContent = currentInput;
+        }
+    }
+    else if (previousButton === '+' || previousButton === '-' || previousButton === '*' || previousButton === '/')
+    {
+        if (secondValue === '') //part of first calculation
+        {
+            secondValue = currentInput;
+            currentInput += key;
+            displayValue.textContent = currentInput;
+            displayEquation.textContent = `${secondValue} ${operator}`; 
+        }
+        else if (secondValue !== '')
+        {
+            currentInput += key;
+            displayValue.textContent = currentInput;
+        }
+    }
+    else
+    {
+        currentInput += key;
+        displayValue.textContent = currentInput;
+    }
+    previousButton = key;
+    log(currentInput, operator, secondValue, displayValue.textContent, displayEquation.textContent, previousButton);
+}
+function operation(key) {
+
+    if (previousButton === '=')
+    {
+        //secondValue = 
+    }
+    else if (previousButton === '+' || previousButton === '-' || previousButton === '*' || previousButton === '/')
+    { //if spamming operator button, calculate once if there are both value.
+        if (currentInput !== '' && secondValue !== '')
+        {
+            const val = `${operate(operator, Number(secondValue), Number(currentInput))}`;
+            secondValue = val;
+            currentInput = '';
+            displayEquation.textContent = val;
+        }
+        else if (currentInput === '' && secondValue !== '')
+        {
+            displayValue.textContent = secondValue;
+            displayEquation.textContent = `${secondValue} ${key}`;
+        }
+        else if (currentInput !== '' && secondValue === '')
+        {
+
+        }
+        else
+        {
+
+            displayEquation.textContent = `${displayValue.textContent} ${key}`;
+        }
+    }
+    else if (previousButton === '')
+    {
+        secondValue = '0';
+        displayValue.textContent = '0';
+        displayEquation.textContent = `0 ${key}`;
+    }
+    else //if //(pB >= 0 && pB <= 9) //if previous was a digits button
+    {
+        if (operator === '')
+        {
+            secondValue = currentInput;
+            displayEquation.textContent = `${secondValue} ${key}`;
+            currentInput = '';
+        }
+        else
+        {
+            if (currentInput !== '' && secondValue !== '')
+            {
+                const val = `${operate(operator, Number(secondValue), Number(currentInput))}`;
+                currentInput = '';
+                secondValue = val;
+                displayEquation.textContent = `${val} ${key}`;
+                displayValue.textContent = val;
+            }
+            else if (currentInput !== '' && secondValue === '')
+            {
+    
+            }
+            else if (currentInput === '' && secondValue !== '')
+            {
+    
+            }
+            else
+            {
+                secondValue = currentInput;
+                currentInput = '';
+                displayEquation.textContent = `${secondValue} ${key}`;
+            }    
+        }
+    }
+    operator = key;
+    previousButton = key;
+    log(currentInput, operator, secondValue, displayValue.textContent, displayEquation.textContent, previousButton);
+}
+function equal (key) {
+    if (previousButton === '')
+    {
+        secondValue = '0';
+        displayEquation.textContent = `0 =`;
+    }
+    else if (previousButton === '=')
+    {   
+        if (currentInput !== '' && secondValue !== '')
+        {
+            secondValue = displayValue.textContent;
+            const val = `${operate(operator, Number(secondValue), Number(currentInput))}`;
+            displayEquation.textContent = `${secondValue} ${operator} ${currentInput} =`;
+            displayValue.textContent = val;
+        }
+    }   
+    else if (previousButton === '+' || previousButton === '-' || previousButton === '*' || previousButton === '/')
+    {
+        // try to calculate
+        if (currentInput === '' && secondValue !== '')
+        {
+            currentInput = secondValue;
+        }
+        const val = `${operate(operator, Number(secondValue), Number(currentInput))}`;
+        //secondValue = val;
+        displayEquation.textContent = `${val} ${operator} ${currentInput} =`;
+        displayValue.textContent = val;
+    }
+    else //equal after number
+    {
+        if (operator === '')
+        {
+            secondValue = currentInput;
+            displayValue.textContent = currentInput;
+            displayEquation.textContent = `${currentInput} =`;
+            currentInput = '';
+        }
+        else
+        {
+            const val = `${operate(operator, Number(secondValue), Number(currentInput))}`;
+            displayEquation.textContent = `${secondValue} ${operator} ${currentInput} =`;
+            displayValue.textContent = val;
+        }
+    }
+    previousButton = key;
+    log(currentInput, operator, secondValue, displayValue.textContent, displayEquation.textContent, previousButton);
+}
+
 const numberButton = document.querySelectorAll('.digits');
-numberButton.forEach(button => button.addEventListener('click', () => {
+numberButton.forEach(button => button.addEventListener('click', () => numerals(button.value)));
+
     /* what to do when digits are pressed after other classes
     if previous was equal button, clear everything and starts new calculation
     if previous was operator button, set second to input if cleared, otherwise, 
         make calculation and set second to new total, then clear currentInput ready for new input
     if keep adding to currentInput
     */
-    if (previousButton === '=')
+    /* if (previousButton === '=')
     {
         if (secondValue === '0')
         {
@@ -61,33 +257,19 @@ numberButton.forEach(button => button.addEventListener('click', () => {
         currentInput += button.value;
         displayValue.textContent = currentInput;
     }
-
-    /* if (resetValue === true) //reset everything right after clicking equal
-    {
-        clear();
-        resetValue = false;
-    }
-    if (resetEquation === true) //clear the calculator after clicking on equal
-    {
-        currentInput = '';
-        displayValue.textContent = '';
-        secondValue = '';
-        displayEquation.textContent = '';
-        resetEquation = false;
-    } */
-    //currentInput += button.value; //update input
-    //displayValue.textContent = currentInput; //update display with input
     previousButton = button.value;
-    log(currentInput, operator, secondValue, displayValue.textContent, displayEquation.textContent, previousButton);
-}));
+    log(currentInput, operator, secondValue, displayValue.textContent, displayEquation.textContent, previousButton); */
+//}));
 
 const operatorButton = document.querySelectorAll('.operator');
-operatorButton.forEach(button => button.addEventListener('click', () => {
+operatorButton.forEach(button => button.addEventListener('click', () => operation(button.value)));
+
+//operatorButton.forEach(button => button.addEventListener('click', () => {
     /* What to do when operator are click after other classes
     if previous was digits, update second to current
     */
     //const pB = Number(previousButton);
-    if (previousButton === '=')
+    /* if (previousButton === '=')
     {
         //secondValue = 
     }
@@ -154,7 +336,7 @@ operatorButton.forEach(button => button.addEventListener('click', () => {
                 displayEquation.textContent = `${secondValue} ${button.value}`;
             }    
         }
-    }
+    } */
 
 
     /* if (resetEquation === true)
@@ -189,19 +371,20 @@ operatorButton.forEach(button => button.addEventListener('click', () => {
     operator = button.value;
     displayEquation.textContent = `${secondValue} ${operator} `; //update equation */
     //resetValue = true;
-    operator = button.value;
-    previousButton = button.value;
-    log(currentInput, operator, secondValue, displayValue.textContent, displayEquation.textContent, previousButton);
-}));
+   // operator = button.value;
+    //previousButton = button.value;
+    //log(currentInput, operator, secondValue, displayValue.textContent, displayEquation.textContent, previousButton);
+//}));
 
 const equalButton = document.querySelector('.equal');
-equalButton.addEventListener('mousedown', () => {
+equalButton.addEventListener('mousedown', () => equal(equalButton.value));
+//equalButton.addEventListener('mousedown', () => {
     /*what to do when click on equal
     if click before anything else, set to 0=
     if click after operator, calculate based on existing values
     if click after number without operator, set to number=
     */
-    if (previousButton === '')
+/*     if (previousButton === '')
     {
         secondValue = '0';
         displayEquation.textContent = `0 =`;
@@ -244,21 +427,9 @@ equalButton.addEventListener('mousedown', () => {
             displayValue.textContent = val;
         }
     }
-
-/*     if (currentInput === '' && operator === '') //clicking enter without all variables
-    {
-        //const val = `${operate(operator, secondValue, currentInput)}`
-    } */
-/*     resetValue = true;
-    resetEquation = true;
-    const val = `${operate(operator, Number(secondValue), Number(currentInput))}`;
-    displayValue.textContent = val;
-    displayEquation.textContent = `${secondValue} ${operator} ${currentInput} =`;
-    secondValue = val; //update secondValue to new total
-    //currentInput = ''; //clear current value */
     previousButton = equalButton.value;
     log(currentInput, operator, secondValue, displayValue.textContent, displayEquation.textContent, previousButton);
-})
+}) */
 
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', clear);
